7160e99c8cd506ba29652489cebe5da543ce773e
sicklyman <totalrubbish2@gmail.com>
2026-05-02 10:20:37 +0100

    Add open check-in button and live auto-refresh to admin dashboard

---
+++ _nuxt/DRk76y35.js
@@ -0,0 +1,1 @@
+import"./Bm3hELW6.js";const s=globalThis.setInterval;export{s};
--- _nuxt/Dw3ycPYZ.js
+++
@@ -1,1 +0,0 @@
-import{Q as a,R as s,S as r,N as u,T as o}from"./CWdLG1tj.js";function i(e){const t=e||s();return t?.ssrContext?.head||t?.runWithContext(()=>{if(r())return u(o)})}function x(e,t={}){const n=i(t.nuxt);if(n)return a(e,{head:n,...t})}export{x as u};
